06df83e5a19437633fa125c8230d57f1ddd4c2e1
chris <chris@DESKTOP-88RKBTU>
2017-04-23 20:21:08 +0200

    test

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
+	console.log("kein test");
 	socket.on('new user', function(data, callback) {
 		if (data in users) {
 			callback(false);
