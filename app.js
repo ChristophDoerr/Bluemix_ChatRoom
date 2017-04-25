aecd3de14fd8f12fb89a2be7ddec762188592114
chris <chris@DESKTOP-88RKBTU>
2017-04-25 18:13:26 +0200

    ...

--- app.js
+++ app.js
@@ -45,14 +45,14 @@ io.sockets.on('connection', function(socket) {
 		} else {
 			callback(true);
 			
-			db.get(username, function(err, dataGet) {
-				if (!err){
-					  console.log(dataGet.id);
+		//	db.get(username, function(err, dataGet) {
+			//	if (!err){
+				//	  console.log(dataGet.id);
 					
 				
-				  }else{
-					  console.log(dataGet.id);
-				  }});
+				//  }else{
+			//		  console.log(dataGet.id);
+			//	  }});
 
 			socket.nickname = data;
 			users[socket.nickname] = socket;
