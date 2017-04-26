dac6e85cd9c24364066c7874a34afb41ee492a8a
chris <chris@DESKTOP-88RKBTU>
2017-04-26 13:25:19 +0200

    t

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ var appEnv = cfenv.getAppEnv();
 
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+//var server = require('http').createServer(app);
+
+var server = require('https').createServer(options, app).listen(443);
 var io = require('socket.io').listen(server);
 var users = {};
 
