1d7390e9b2395b44ef8b44fa8dead3e85963db4c
chris <chris@DESKTOP-88RKBTU>
2017-04-26 13:30:06 +0200

    test

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var express = require('express');
 var app = express();
 //var server = require('http').createServer(app);
 
-var server = require('https').createServer(options, app).listen(443);
+var server = require('https').createServer(app);
 var io = require('socket.io').listen(server);
 var users = {};
 
