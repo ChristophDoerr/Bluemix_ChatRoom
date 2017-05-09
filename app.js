9383c81acd19a8cdd4096016e3591145e49287c9
chris <chris@DESKTOP-88RKBTU>
2017-05-10 00:04:45 +0200

    ...

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ server.listen(8080);
 
 var cfEnv = require('cf-env');
 var cfenv = require('cfenv');
-var appEnv = cfenv.getAppEnv();
+var appEnv = cfEnv.getAppEnv();
 var pkg   = require("./package.json");
   var dbCreds =  appEnv.getServiceCreds('Cloudant NoSQL DB-08');  
   var nano;
